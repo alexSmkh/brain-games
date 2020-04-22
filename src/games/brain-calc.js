0457d3cd03835329cc6e233bb34dc632e9171979
alexSmkh <alexshikari9333@gmail.com>
2020-04-22 12:27:57 +0300

    Moved math funcs to math-lib.js

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,41 +1,13 @@
 import pairs from '@hexlet/pairs';
-import { getRandomNumber, getRandomOperator } from '../math-lib.js';
+import {
+  getPairOfNumbers, getFirstNumber, getSecondNumber, getOperatorOfExpression,
+  makeMathExpression, solveExpression,
+} from '../math-lib.js';
 
 export const getBrainCalcRules = () => 'What is the result of the expression?';
 
-const makePairOfNumbers = () => pairs.cons(getRandomNumber(), getRandomNumber());
-
-const makeMathExpression = () => pairs.cons(getRandomOperator(), makePairOfNumbers());
-
-const getOperatorOfExpression = (expression) => pairs.car(expression);
-
-const getPairsOfNumbers = (expression) => pairs.cdr(expression);
-
-const getFirstNumber = (pairsOfNumbers) => pairs.car(pairsOfNumbers);
-
-const getSecondNumber = (pairsOfNumbers) => pairs.cdr(pairsOfNumbers);
-
-const solveExpression = (expression) => {
-  let result;
-  const pairsOfNumbers = getPairsOfNumbers(expression);
-  switch (getOperatorOfExpression(expression)) {
-    case '+':
-      result = getFirstNumber(pairsOfNumbers) + getSecondNumber(pairsOfNumbers);
-      break;
-    case '-':
-      result = getFirstNumber(pairsOfNumbers) - getSecondNumber(pairsOfNumbers);
-      break;
-    case '*':
-      result = getFirstNumber(pairsOfNumbers) * getSecondNumber(pairsOfNumbers);
-      break;
-    default:
-      break;
-  }
-  return result;
-};
-
 const makeQuestionForChallenge = (expression) => {
-  const pairsOfNumbers = getPairsOfNumbers(expression);
+  const pairsOfNumbers = getPairOfNumbers(expression);
   const firstNumber = getFirstNumber(pairsOfNumbers);
   const secondNumber = getSecondNumber(pairsOfNumbers);
   const operator = getOperatorOfExpression(expression);
--- src/math-lib.js
+++ src/math-lib.js
@@ -1,3 +1,5 @@
+import pairs from '@hexlet/pairs';
+
 export const isEven = (number) => number % 2 === 0;
 
 export const getRandomNumber = (
@@ -21,4 +23,40 @@ export const getRandomOperator = () => {
       break;
   }
   return randomOperator;
+};
+
+export const makePairOfNumbers = () => pairs.cons(getRandomNumber(), getRandomNumber());
+
+export const getFirstNumber = (pairsOfNumbers) => pairs.car(pairsOfNumbers);
+
+export const getSecondNumber = (pairsOfNumbers) => pairs.cdr(pairsOfNumbers);
+
+export const makeMathExpression = () => pairs.cons(getRandomOperator(), makePairOfNumbers());
+
+export const getOperatorOfExpression = (expression) => pairs.car(expression);
+
+export const getPairOfNumbers = (expression) => pairs.cdr(expression);
+
+export const solveExpression = (expression) => {
+  let result;
+  const pairsOfNumbers = getPairOfNumbers(expression);
+  switch (getOperatorOfExpression(expression)) {
+    case '+':
+      result = getFirstNumber(pairsOfNumbers) + getSecondNumber(pairsOfNumbers);
+      break;
+    case '-':
+      result = getFirstNumber(pairsOfNumbers) - getSecondNumber(pairsOfNumbers);
+      break;
+    case '*':
+      result = getFirstNumber(pairsOfNumbers) * getSecondNumber(pairsOfNumbers);
+      break;
+    default:
+      break;
+  }
+  return result;
+};
+
+export const getGcd = (num1, num2) => {
+  if (num2 === 0) return num1;
+  return getGcd(num2, num1 % num2);
 };
