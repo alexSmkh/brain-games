7a9c8bb69d7c850ed62c79f58a4818e7c0bdaa08
alexSmkh <alexshikari9333@gmail.com>
2020-04-29 19:58:00 +0300

    Deleted console.log(...) wrappers

--- src/index.js
+++ src/index.js
@@ -1,57 +1,37 @@
 import pairs from '@hexlet/pairs';
 import readlineSync from 'readline-sync';
 
-const greeting = () => console.log('Welcome to the Brain Games!\n');
-
-const getUsername = () => readlineSync.question('May I have your name? ');
-
-const sayHello = (username) => console.log(`Hello, ${username}!\n`);
-
-const explainRules = (rules) => console.log(`${rules}\n`);
-
-const getUserAnswer = () => readlineSync.question('Your answer: ');
-
-const askQuestion = (question) => console.log(`Question: ${question}`);
-
-const congratulate = (username) => console.log(`Congratulations, ${username}!\n`);
-
-const sayTryAgain = (username) => console.log(`Let's try again, ${username}!\n`);
-
-const sayCorrect = () => console.log('Correct!\n');
-
-const sayCorrectAnswer = (userAnswer, correctAnswer) => console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".\n`);
-
 const getQuestion = (challenge) => pairs.car(challenge);
 
 const getAnswer = (challenge) => pairs.cdr(challenge);
 
-const runGame = (getRoundData, score = 0) => {
+const runGameRound = (getRoundData, score = 0) => {
   const numberRounds = 3;
   if (score === numberRounds) return true;
 
   const challenge = getRoundData();
-  askQuestion(getQuestion(challenge));
+  console.log(`Question: ${getQuestion(challenge)}`);
 
-  const userAnswer = getUserAnswer();
+  const userAnswer = readlineSync.question('Your answer: ');
   const correctAnswer = String(getAnswer(challenge));
   const scoreForAnswer = (userAnswer === correctAnswer ? 1 : 0);
   if (!scoreForAnswer) {
-    sayCorrectAnswer(userAnswer, getAnswer(challenge));
+    console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".\n`);
     return false;
   }
-  sayCorrect();
-  return runGame(getRoundData, score + 1);
+  console.log('Correct!\n');
+  return runGameRound(getRoundData, score + 1);
 };
 
 const main = (getRoundDataFunc, gameRules) => {
-  greeting();
-  const username = getUsername();
-  sayHello(username);
-  explainRules(gameRules);
+  console.log('Welcome to the Brain Games!\n');
+  const username = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${username}!\n`);
+  console.log(`${gameRules}\n`);
 
-  const gameResult = runGame(getRoundDataFunc);
-  if (gameResult) congratulate(username);
-  else sayTryAgain(username);
+  const gameResult = runGameRound(getRoundDataFunc);
+  if (gameResult) console.log(`Congratulations, ${username}!\n`);
+  else console.log(`Let's try again, ${username}!\n`);
 };
 
 export default main;
