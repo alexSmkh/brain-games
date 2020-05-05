fe6033dcf2af6cf75f5854a8e200a2013d352e74
alexSmkh <alexshikari9333@gmail.com>
2020-05-06 01:02:56 +0300

    Removed unnecessary functions

--- src/utils.js
+++ src/utils.js
@@ -5,15 +5,6 @@ export const getRandomNumber = (
   max = 100,
 ) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-export const makePairOfNumbers = (num1 = null, num2 = null) => {
-  if (num1 === null) return pairs.cons(getRandomNumber(), getRandomNumber());
-  return pairs.cons(num1, num2);
-};
-
-export const getFirstNumberOfPair = (pairsOfNumbers) => pairs.car(pairsOfNumbers);
-
-export const getSecondNumberOfPair = (pairsOfNumbers) => pairs.cdr(pairsOfNumbers);
-
 export const makeRoundData = (question, answer) => pairs.cons(question, answer);
 
 export const getQuestion = (questionAndAnswer) => pairs.car(questionAndAnswer);
