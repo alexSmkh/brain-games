da6f511a879b61baf23952b56246d22eb51a8e84
alexSmkh <alexshikari9333@gmail.com>
2020-05-05 21:48:42 +0300

    Changed logic for runGameRound func

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import readlineSync from 'readline-sync';
 import { getQuestion, getAnswer } from './utils.js';
 
-const runGameRound = (getRoundData, score = 0) => {
-  const numberRounds = 3;
-  if (score === numberRounds) return true;
+const numberRounds = 3;
 
-  const challenge = getRoundData();
-  console.log(`Question: ${getQuestion(challenge)}`);
-
+const runGameRound = (getRoundData, username, score = 0) => {
+  if (score === numberRounds) {
+    console.log(`Congratulations, ${username}!\n`);
+    return;
+  }
+  const roundData = getRoundData();
+  console.log(`Question: ${getQuestion(roundData)}`);
   const userAnswer = readlineSync.question('Your answer: ');
-  const correctAnswer = String(getAnswer(challenge));
-  const scoreForAnswer = (userAnswer === correctAnswer ? 1 : 0);
-  if (!scoreForAnswer) {
+  const correctAnswer = String(getAnswer(roundData));
+  if (!(userAnswer === correctAnswer)) {
     console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".\n`);
-    return false;
+    console.log(`Let's try again, ${username}!\n`);
   }
   console.log('Correct!\n');
-  return runGameRound(getRoundData, score + 1);
+  runGameRound(getRoundData, username, score + 1);
 };
 
 const runGameEngine = (getRoundData, gameDescription) => {
@@ -24,10 +25,7 @@ const runGameEngine = (getRoundData, gameDescription) => {
   const username = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${username}!\n`);
   console.log(`${gameDescription}\n`);
-
-  const gameResult = runGameRound(getRoundData);
-  if (gameResult) console.log(`Congratulations, ${username}!\n`);
-  else console.log(`Let's try again, ${username}!\n`);
+  runGameRound(getRoundData, username);
 };
 
 export default runGameEngine;
