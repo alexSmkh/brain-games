ce8c98e1b3da88af5ce00635b4f07b5136f74b2a
alexSmkh <alexshikari9333@gmail.com>
2020-05-05 22:34:10 +0300

    Changed logic for making round data

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,7 +1,4 @@
-import {
-  getFirstNumberOfPair, getSecondNumberOfPair, getRandomNumber,
-  makePairOfNumbers, makeRoundData,
-} from '../utils.js';
+import { getRandomNumber, makeRoundData } from '../utils.js';
 import runGameEngine from '../index.js';
 
 const gameDescription = 'Find the greatest common divisor of given numbers.';
@@ -11,24 +8,17 @@ const getGcd = (num1, num2) => {
   return getGcd(num2, num1 % num2);
 };
 
-const makeQuestionForChallenge = (
-  pairOfNumbers,
-) => `${getFirstNumberOfPair(pairOfNumbers)} ${getSecondNumberOfPair(pairOfNumbers)}`;
-
 const makeDecreasingPairOfNumbers = () => {
   const num1 = getRandomNumber();
   const num2 = getRandomNumber();
-  return (num1 > num2) ? makePairOfNumbers(num1, num2) : makePairOfNumbers(num2, num1);
+  return (num1 > num2) ? [num1, num2] : [num2, num1];
 };
 
 const getRoundData = () => {
-  const pairOfNumbers = makeDecreasingPairOfNumbers();
-  const questionForChallenge = makeQuestionForChallenge(pairOfNumbers);
-  const answer = String(getGcd(
-    getFirstNumberOfPair(pairOfNumbers),
-    getSecondNumberOfPair(pairOfNumbers),
-  ));
-  return makeRoundData(questionForChallenge, answer);
+  const [num1, num2] = makeDecreasingPairOfNumbers();
+  const question = `${num1} ${num2}`;
+  const answer = String(getGcd(num1, num2));
+  return makeRoundData(question, answer);
 };
 
 export default () => {
