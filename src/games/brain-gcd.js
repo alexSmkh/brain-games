e939f1332ae7732659c65e1fe227bfcc20a57c3c
alexSmkh <alexshikari9333@gmail.com>
2020-05-12 14:30:55 +0300

    Changed the function for getting gcd number

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -4,18 +4,14 @@ import runGameEngine from '../index.js';
 const gameDescription = 'Find the greatest common divisor of given numbers.';
 
 const getGcd = (num1, num2) => {
-  if (num2 === 0) return num1;
-  return getGcd(num2, num1 % num2);
-};
-
-const makeDecreasingPairOfNumbers = () => {
-  const num1 = getRandomNumber();
-  const num2 = getRandomNumber();
-  return (num1 > num2) ? [num1, num2] : [num2, num1];
+  if (num2 === 0 || num1 === 0) return num1 + num2;
+  if (num1 > num2) return getGcd(num2, num1 % num2);
+  return getGcd(num1, num2 % num1);
 };
 
 const getRoundData = () => {
-  const [num1, num2] = makeDecreasingPairOfNumbers();
+  const num1 = getRandomNumber();
+  const num2 = getRandomNumber();
   const question = `${num1} ${num2}`;
   const answer = String(getGcd(num1, num2));
   return makeRoundData(question, answer);
