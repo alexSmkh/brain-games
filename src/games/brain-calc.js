9e7baf641ea0f24b52c3a0d2f1d1c65d4ef2952c
alexSmkh <alexshikari9333@gmail.com>
2020-05-12 17:06:54 +0300

    Changed getting a question for the getRoundData func

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -20,23 +20,11 @@ const solveExpression = (operator, numbers) => {
   }
 };
 
-const makeQuestion = (operator, numbers) => {
-  const partsOfQuestion = numbers.reduce((acc, number, index, arr) => {
-    if (index >= arr.length - 1) {
-      acc.push(number);
-    } else {
-      acc.push(number, operator);
-    }
-    return acc;
-  }, []);
-  return partsOfQuestion.join(' ');
-};
-
 const getRoundData = () => {
   const countOfNumbers = 2;
   const numbers = _.times(countOfNumbers, getRandomNumber);
   const mathOperator = mathOperators[getRandomNumber(mathOperators.length - 1)];
-  const question = makeQuestion(mathOperator, numbers);
+  const question = numbers.join(`${mathOperator}`);
   const answer = String(solveExpression(mathOperator, numbers));
   return makeRoundData(question, answer);
 };
